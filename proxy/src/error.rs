d2dc64b9dcca02f72d6d502506ed537b080e4530
Qu Hao <quhxuxm_quh@163.com>
2021-12-29 21:27:44 +0800

    init

--- proxy/src/error.rs
+++ proxy/src/error.rs
@@ -4,14 +4,16 @@ use ppaass_common::agent::PpaassAgentMessagePayloadType;
 use ppaass_common::common::PpaassAddress;
 use ppaass_common::error::PpaassCommonError;
 
-use crate::transport::TcpTransportStatus;
+use crate::transport::TransportStatus;
 
 #[derive(thiserror::Error, Debug)]
 pub(crate) enum PpaassProxyError {
     #[error("Connect to target fail")]
     ConnectToTargetFail(#[from] std::io::Error),
     #[error("Invalid tcp transport status, Transport: [{0}] require status: {1:#?}, get status: {2:#?}")]
-    InvalidTcpTransportStatus(String, TcpTransportStatus, TcpTransportStatus),
+    InvalidTcpTransportStatus(String, TransportStatus, TransportStatus),
+    #[error("Invalid udp transport status, Transport: [{0}] require status: {1:#?}, get status: {2:#?}")]
+    InvalidUdpTransportStatus(String, TransportStatus, TransportStatus),
     #[error("Receive invalid agent message, Transport: [{0}] require status: {1:#?}, get status: {2:#?}")]
     ReceiveInvalidAgentMessage(String, PpaassAgentMessagePayloadType, PpaassAgentMessagePayloadType),
     #[error("Unknown error happen")]
--- proxy/src/server.rs
+++ proxy/src/server.rs
@@ -12,72 +12,54 @@ use tokio::net::TcpListener;
 use tokio::runtime::Runtime;
 
 use crate::config::ProxyConfiguration;
-use crate::transport::{TcpTransport, TcpTransportSnapshot, TcpTransportStatus};
+use crate::transport::{Transport, TransportSnapshot, TransportStatus};
 
 const CONFIG_FILE_PATH: &str = "ppaass-proxy.toml";
-pub const LOCAL_ADDRESS: [u8; 4] = [0u8; 4];
+const LOCAL_ADDRESS: [u8; 4] = [0u8; 4];
 
 const AGENT_PUBLIC_KEY_PATH: &str = "AgentPublicKey.pem";
 const PROXY_PRIVATE_KEY_PATH: &str = "ProxyPrivateKey.pem";
-
 
 pub struct Server {
     master_runtime: Runtime,
     worker_runtime: Arc<Runtime>,
     configuration: Arc<ProxyConfiguration>,
-    transports: Arc<RwLock<HashMap<String, TcpTransportSnapshot>>>,
+    transports: Arc<RwLock<HashMap<String, TransportSnapshot>>>,
 }
-
 
 impl Server {
     pub fn new() -> Result<Self> {
         let mut config_file = File::open(CONFIG_FILE_PATH)?;
         let mut config_file_content = String::new();
         config_file.read_to_string(&mut config_file_content)?;
-        let proxy_server_config = toml::from_str::<ProxyConfiguration>(&config_file_content)
-            .with_context(|| "Fail to parse proxy configuration file.")?;
+        let proxy_server_config = toml::from_str::<ProxyConfiguration>(&config_file_content).with_context(|| "Fail to parse proxy configuration file.")?;
         log4rs::init_file(
             proxy_server_config.log_config().as_ref().unwrap(),
             Default::default(),
-        )
-            .with_context(|| "Fail to initialize proxy configuration file.")?;
+        ).with_context(|| "Fail to initialize proxy configuration file.")?;
         let mut master_runtime_builder = tokio::runtime::Builder::new_multi_thread();
         master_runtime_builder.worker_threads(
-            proxy_server_config
-                .master_thread_number()
-                .with_context(|| "Can not get worker threads number from proxy configuration.")?,
+            proxy_server_config.master_thread_number().with_context(|| "Can not get worker threads number from proxy configuration.")?,
         );
         master_runtime_builder.max_blocking_threads(
-            proxy_server_config
-                .max_blocking_threads()
-                .with_context(|| "Can not get max blocking threads number from proxy configuration.")?,
+            proxy_server_config.max_blocking_threads().with_context(|| "Can not get max blocking threads number from proxy configuration.")?,
         );
         master_runtime_builder.thread_name("proxy-master");
         master_runtime_builder.thread_keep_alive(Duration::from_secs(
-            proxy_server_config
-                .thread_timeout()
-                .with_context(|| "Can not get thread timeout from proxy configuration.")?,
+            proxy_server_config.thread_timeout().with_context(|| "Can not get thread timeout from proxy configuration.")?,
         ));
         master_runtime_builder.enable_all();
-        let master_runtime = master_runtime_builder
-            .build()
-            .with_context(|| "Fail to build init tokio runtime.")?;
+        let master_runtime = master_runtime_builder.build().with_context(|| "Fail to build init tokio runtime.")?;
         let mut worker_runtime_builder = tokio::runtime::Builder::new_multi_thread();
         worker_runtime_builder.worker_threads(
-            proxy_server_config
-                .worker_thread_number()
-                .with_context(|| "Can not get relay thread number from proxy configuration.")?,
+            proxy_server_config.worker_thread_number().with_context(|| "Can not get relay thread number from proxy configuration.")?,
         );
         worker_runtime_builder.max_blocking_threads(
-            proxy_server_config
-                .max_blocking_threads()
-                .with_context(|| "Can not get max blocking threads number from proxy configuration.")?,
+            proxy_server_config.max_blocking_threads().with_context(|| "Can not get max blocking threads number from proxy configuration.")?,
         );
         worker_runtime_builder.thread_name("proxy-worker");
         worker_runtime_builder.thread_keep_alive(Duration::from_secs(
-            proxy_server_config
-                .thread_timeout()
-                .with_context(|| "Can not get thread time out from proxy configuration.")?,
+            proxy_server_config.thread_timeout().with_context(|| "Can not get thread time out from proxy configuration.")?,
         ));
         worker_runtime_builder.enable_all();
         let worker_runtime = worker_runtime_builder.build()?;
@@ -94,7 +76,7 @@ impl Server {
         let proxy_private_key = std::fs::read_to_string(Path::new(PROXY_PRIVATE_KEY_PATH)).expect("Fail to read proxy private key.");
         let proxy_server_config = self.configuration.clone();
         let worker_runtime = self.worker_runtime.clone();
-        let (transport_info_sender, mut transport_info_receiver) = tokio::sync::mpsc::channel::<TcpTransportSnapshot>(32);
+        let (transport_info_sender, mut transport_info_receiver) = tokio::sync::mpsc::channel::<TransportSnapshot>(32);
         let transports = self.transports.clone();
         self.master_runtime.spawn(async move {
             loop {
@@ -111,7 +93,7 @@ impl Server {
                                 continue;
                             }
                             Ok(transports) => {
-                                if snapshot.status == TcpTransportStatus::Closed {
+                                if snapshot.status == TransportStatus::Closed {
                                     info!("Remove closed transport, transport: [{}]", snapshot.id);
                                     transports.remove(snapshot.id.as_str());
                                     continue;
@@ -124,7 +106,6 @@ impl Server {
                 }
             }
         });
-
         let transports = self.transports.clone();
         self.master_runtime.spawn(async move {
             let mut interval = tokio::time::interval(Duration::from_secs(30));
@@ -148,17 +129,13 @@ impl Server {
                 interval.tick().await;
             }
         });
-
         self.master_runtime.block_on(async move {
             let local_port = proxy_server_config.port().unwrap();
             let local_ip = IpAddr::from(LOCAL_ADDRESS);
             let local_address = SocketAddr::new(local_ip, local_port);
-            let tcp_listener = TcpListener::bind(local_address)
-                .await
-                .unwrap_or_else(|e| panic!("Fail to start proxy because of error, error: {:#?}", e));
+            let tcp_listener = TcpListener::bind(local_address).await.unwrap_or_else(|e| panic!("Fail to start proxy because of error, error: {:#?}", e));
             //Start to processing client protocol
             info!("Success to bind TCP server on port: [{}]", local_port);
-
             loop {
                 let agent_connection_accept_result = tcp_listener.accept().await;
                 if let Err(e) = agent_connection_accept_result {
@@ -173,24 +150,24 @@ impl Server {
                 let agent_public_key = agent_public_key.clone();
                 let proxy_private_key = proxy_private_key.clone();
                 worker_runtime.spawn(async move {
-                    let tcp_transport = TcpTransport::new(agent_remote_addr,
-                                                          transport_info_sender.clone());
-                    if let Err(e) = tcp_transport {
+                    let transport = Transport::new(agent_remote_addr,
+                        transport_info_sender.clone());
+                    if let Err(e) = transport {
                         error!("Fail to create agent tcp transport because of error, error: {:#?}",e );
                         return;
                     }
-                    let mut tcp_transport = tcp_transport.unwrap();
-                    let tcp_transport_id = tcp_transport.id().to_string();
-                    info!("Receive a agent stream from: [{}], assign it to transport: [{}].", agent_remote_addr, tcp_transport_id);
-                    if let Err(e) = tcp_transport.start(agent_stream, agent_public_key, proxy_private_key).await {
-                        error!("Fail to start agent tcp transport because of error, transport:[{}], agent address:[{}], error: {:#?}",tcp_transport_id,
+                    let mut transport = transport.unwrap();
+                    let transport_id = transport.id().to_string();
+                    info!("Receive a agent stream from: [{}], assign it to transport: [{}].", agent_remote_addr, transport_id);
+                    if let Err(e) = transport.start(agent_stream, agent_public_key, proxy_private_key).await {
+                        error!("Fail to start agent tcp transport because of error, transport:[{}], agent address:[{}], error: {:#?}",transport_id,
                             agent_remote_addr,e);
                     }
-                    if let Err(e) = tcp_transport.close().await {
-                        error!("Fail to close agent tcp transport because of error, transport:[{}], agent address:[{}], error: {:#?}",tcp_transport_id,
+                    if let Err(e) = transport.close().await {
+                        error!("Fail to close agent tcp transport because of error, transport:[{}], agent address:[{}], error: {:#?}",transport_id,
                             agent_remote_addr,e);
                     }
-                    info!("Graceful close agent tcp transport: [{}]", tcp_transport_id);
+                    info!("Graceful close agent tcp transport: [{}]", transport_id);
                 });
             }
         });
