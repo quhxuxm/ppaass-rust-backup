3bddfb8fcaf17244dd3f5946ad3b92fe4d493889
quh <quhxuxm_quh@163.com>
2022-01-04 15:37:33 +0800

    Refact: add logs

---
+++ udptesting/src/udpclient.py
@@ -0,0 +1,22 @@
+import socket
+
+import socks
+
+if __name__ == "__main__":
+    sock = socks.socksocket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.set_proxy(socks.SOCKS5, "10.175.4.220", 10081)
+    # sock.bind(("0.0.0.0", 8701))
+    sock.connect(("10.175.4.220", 8700))
+    print('Start client: [%s]', sock)
+
+    i = 0
+
+    while True:
+        message = bytes('This is client message.  It will be repeated: %s' % i, 'utf-8')
+        i = i + 1
+        print('sending [%s]' % message)
+        sock.send(message)
+        data = sock.recv(65535)
+        print('received [%s]' % data)
+
+    sock.close()
---
+++ udptesting/src/udpserver.py
@@ -0,0 +1,19 @@
+import socket
+
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("0.0.0.0", 8700))
+    print('Start listening udp: [%s]', sock)
+
+    i = 0
+
+    while True:
+
+        message, client_address = sock.recvfrom(65535)
+        print('connection from', client_address)
+        print('received "%s"' % message)
+        print('sending data back to the client [%s]' % i)
+        sock.sendto(bytes("server echo: [%s]" % i, "utf-8"), client_address)
+        print('finish', client_address)
+        i = i + 1
