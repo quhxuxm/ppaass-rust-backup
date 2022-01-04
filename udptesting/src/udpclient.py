dd0d24521de26ed1e7a521ec45f34fe2066c7134
quh <quhxuxm_quh@163.com>
2022-01-04 16:43:00 +0800

    Refact: add logs

--- udptesting/src/udpclient.py
+++ udptesting/src/udpclient.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     # sock.bind(("0.0.0.0", 8701))
     sock.connect(("10.175.4.220", 8700))
     print('Start client: [%s]', sock)
-
     i = 0
-
     while True:
         message = bytes('This is client message.  It will be repeated: %s' % i, 'utf-8')
         i = i + 1
--- udptesting/src/udpserver.py
+++ udptesting/src/udpserver.py
@@ -1,15 +1,11 @@
 import socket
 
-
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", 8700))
     print('Start listening udp: [%s]', sock)
-
     i = 0
-
     while True:
-
         message, client_address = sock.recvfrom(65535)
         print('connection from', client_address)
         print('received "%s"' % message)
