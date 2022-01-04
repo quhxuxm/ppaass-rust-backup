a8ad721c5921b2f18005ac76b32157300d6be99b
quh <quhxuxm_quh@163.com>
2022-01-04 16:48:26 +0800

    Refact: add logs

--- udptesting/src/udpserver.py
+++ udptesting/src/udpserver.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     i = 0
     while True:
         message, client_address = sock.recvfrom(65535)
-        print('connection from', client_address)
+        print('connection from: ', client_address)
         print('received "%s"' % message)
         print('sending data back to the client [%s]' % i)
         # sock.send(bytes("server echo: [%s]" % i, "utf-8"))
